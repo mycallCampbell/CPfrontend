ce267676f7b256ebfd0a0c38ccebb05019489887
unknown <mycallcampbell@gmail.com>
2023-04-16 01:44:43 +0100

    corrected name in component

--- components/TitleHero.js
+++ components/TitleHero.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./TitleHero.module.css";
 
-export default function ({ title }) {
+export default function TitleHero({ title }) {
   return (
     <div>
       <div className={styles.title}>{title}</div>
