26053466f62436c7110a8e2fc15f6421bc2c82ba
MycallDev <mycalldev@localhost.localdomain>
2023-07-08 21:04:42 +0100

    added more to learning blog

--- pages/learnTolearn.js
+++ pages/learnTolearn.js
@@ -48,13 +48,13 @@ function learnTolearn() {
         <div className={styles.mainContent}>
         If this is the case, then there must be a way to achieve such an objective.  
 
-        Over the years of my learning journey as studying a varying array/list (lol.…) of disciplines - both physical and mental - I have stumbled across a few key factors that the human mind could benefit from. This is especially true when learning a new construct or concept that may require more than discipline to master. 
+        Over the years of my learning journey in studying a variable array/list (lol.…) of disciplines - both physical and mental - I have discovered a few key factors that the human mind could benefit from. This is especially true when learning a new construct or concept that may require more than discipline to master. 
 
         Having said that, ‘Discipline’ is a fundamental part of the overall picture and something we can cover in a short book (some other time). 
 
         </div>
         <div className={styles.mainContent}>
-        What I am getting at is a protocol for learning. A system, per se, for undertaking a subject/discipline and using the aforementioned protocol to assist in the perspicacity and digestion of its inherent contents. 
+        What I'm pertaining to is a protocol for learning. A system, per se, for undertaking a subject/discipline and using the aforementioned protocol to assist in the perspicacity and digestion of its inherent contents. 
         </div>
 
         <div className={styles.mainContent}>
@@ -62,13 +62,14 @@ function learnTolearn() {
         </div>
 
         <div className={styles.mainContent}>
-        The Protocol I have set out is structured into <span>6</span> orderly sections: 
+        The Protocol I have set out is structured into <span>7</span> orderly sections: 
         <ul>
+            <li className={styles.listI}>Relevance and/or context for why we are learning such subject</li>
             <li className={styles.listI}>Explination of the Subject</li>
             <li className={styles.listI}>Analogy</li>
             <li className={styles.listI}>Example</li>
-            <li className={styles.listI}>Relevance and/or context for why we are learning such subject</li>
             <li className={styles.listI}>Deduction - How not to use</li>
+            <li className={styles.listI}>Dive in and start doing</li>
             <li className={styles.listI}>Repetition</li>
         </ul>
         </div>
@@ -81,20 +82,50 @@ function learnTolearn() {
         </div>
 
         <div className={styles.subjectTitle}>
-            Explanation:
+            Relevance
         </div>
-
         <div className={styles.mainContent}>
-        Starting with the ‘Explanation’ will cover new concepts to the mind albeit in a detailed way. This is pertinent to refer back to even though at the time of hearing the explanation some parts (maybe more than others) may not make sense.  
+        Relevance is integral toward informing the brain on why it is you are doing what you are doing (in terms of learning). I remember many times on my journey, setting out to learn a function or construct (such as Database models) and not knowing the relevance/purpose behind why a function or model is required. How do they tie into the bigger picture? What is the bigger picture? These were a few of the many questions my mind was asking itself. I realised very early on that if i was without ‘Relevance’ then I would quickly forget the subject I had recently studied. Having said that, ‘Relevance’ can take some time to acquire (depending on the subject matter). The reason is complex subjects have many moving parts or a large variety of small pieces to a very large puzzle. Eventually, the more pieces of the puzzle you put together, the clearer the bigger picture becomes…. Hence the Relevance…. 
+
+        A lot of material you find in the wild will tell you the ‘How’. In terms of Relevance, you will want to know the ‘Why’. i.e. why am I creating a function or a model and what role does the function or model play. 
+
         </div>
 
         <div className={styles.subjectTitle}>
-            Analogy:
-        </div>
-        <div className={styles.mainContent}>
-        The ‘Analogy’ will use comparisons of material you are familiar with in order to help make sense of the parts of the explanation that you are yet to understand fully. Essentially, analogies are great at making the mind feel comfortable and we can relate and regain our attention span. 
+            Explanation
         </div>
 
+        <div className={styles.mainContent}>
+        The ‘Explanation’ will introduce new concepts to the mind in a detailed way. This is pertinent to refer back to even though at the time of hearing the explanation some parts (maybe more than others) may not make sense.  
+        </div>
+
+        <div className={styles.subjectTitle}>
+            Analogy
+        </div>
+        <div className={styles.mainContent}>
+        The ‘Analogy’ will use comparisons of material you are familiar with in order to help make sense of the parts of the explanation that you are yet to fully understand. Essentially, analogies are great at making the mind feel comfortable and we can relate and regain our attention span. 
+        </div>
+        <div className={styles.subjectTitle}>
+            Example
+        </div>
+        <div className={styles.mainContent}>
+        Once we have absorbed the explanation and analogy, we require an example of how to do! Examples are great and allow the student (us) to observe, and remembering that we now have the foundational knowledge of the explanation and analogy to assist in bringing the pieces of the puzzle together. Watching, reading or listening to an example may give you the infamous 'aha' moment. 
+        </div>
+        <div className={styles.subjectTitle}>
+            Deduction
+        </div>
+        <div className={styles.mainContent}>
+        Deduction is one of my personal favourites. Many times I have studied videos on the internet and the content creator has made a mistake and corrected themselves thereafter. In this moment I realised that seeing how something should not be carried out allows the mind to deduce on how it is that it can only be carried out. In other words, by learning how a construct cannot be applied, will help refine the knowledge of a construct can only be applied. The ‘How not to do’, (as I call it) has helped me significantly in learning the fine details of any given subject. I’m always asking myself: ‘How can this not work…. What are its inherent limitations’.   
+        </div>
+        <div className={styles.subjectTitle}>
+            Dive In
+        </div>
+        <div className={styles.mainContent}>
+        Dive in and start doing (kinesthetic learning) is the one of the most proficient ways to pick up a new subject. Coupled with the prerequisite steps of the protocol, you will be more than ready to roll your sleeves up and travel into ‘key-stroke heaven’. Ok… ok… In truth this requires patience, discipline and practice. The harder and smarter you work at this stage will define the outcome. ‘You get out what you put in’. As simple as that! Having said that, work smart and take breaks often. Your brain only starts to assimilate the newly acquired knowledge and compartmentalise whilst you are resting. Close your eyes for 20 minutes and recharge the mind. We all know how the day goes (cognitively speaking) when we have less sleep than usual. I would also like to mention that parts of our brain work independently at times. To elaborate, when we are learning ‘Syntax’ for a new programming language, the logical mind takes a back seat. The part of your brain that works on memory moves to the forefront to pick up on the syntax. So when trying to iterate over a list or carry out some logic in the initial stages it will feel like you have forgotten how to count as you are primarily focused on the correct syntax. I actually forgot how to spell (for a brief period) during my time learning the syntax for Javascript. In other words, the brain utilises a method known as the ‘separation of concerns’. Only once you have the syntax embedded, does the logical mind come into full force. It takes time, although with enough practice you will receive your ‘aha’ moment. 
+
+
+        </div>
+        
         <div className={styles.sideNote}>
         Side Note: 
         </div>
